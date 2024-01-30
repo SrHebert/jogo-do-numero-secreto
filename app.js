c4f29b7df66562b9ad99167433b6bd705715dfc8
Ronny Hebert <61034508+SrHebert@users.noreply.github.com>
2024-01-29 21:20:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function verificarChute(){
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar'). removeAttribute('disabled');
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else{
         if(chute > numeroSecreto){
             exibirTextoNaTela('p','O número secreto é menor');
